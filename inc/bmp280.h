599abce89902ef652a5df89b067a65acc7895b7a
Reza Ebrahimi <69025534+ebrezadev@users.noreply.github.com>
2025-07-13 00:44:05 +0330

    Update bmp280.h

--- inc/bmp280.h
+++ inc/bmp280.h
@@ -148,14 +148,6 @@ bmp280_error_code_t bmp280_get_filter_coefficient(
 	bmp280_handle_t *handle, 
 	bmp280_iir_filter_t *filter_coeff);
 
-
-int bmp280_i2c_init();
-int bmp280_i2c_deinit();
-int bmp280_read_array(uint8_t deviceAddress, uint8_t startRegisterAddress, uint8_t *data, uint8_t dataLength);
-int bmp280_write_array(uint8_t deviceAddress, uint8_t startRegisterAddress, uint8_t *data, uint8_t dataLength);
-int delay_function(uint32_t delayMS);
-int power_function(float x, float y, float *result);
-
 #ifdef __cplusplus
 }
 #endif
