ed61fb7b02483c2ed325a58e58dbcb98abf79947
Reza Ebrahimi <69025534+ebrezadev@users.noreply.github.com>
2025-07-13 00:50:40 +0330

    Create interface.hpp

---
+++ example/arduino/interface.hpp
@@ -0,0 +1,11 @@
+#ifndef __INTERFACE_HPP__
+#define __INTERFACE_HPP__
+
+int bmp280_i2c_init();
+int bmp280_i2c_deinit();
+int bmp280_read_array(uint8_t deviceAddress, uint8_t startRegisterAddress, uint8_t *data, uint8_t dataLength);
+int bmp280_write_array(uint8_t deviceAddress, uint8_t startRegisterAddress, uint8_t *data, uint8_t dataLength);
+int delay_function(uint32_t delayMS);
+int power_function(float x, float y, float *result);
+
+#endif
