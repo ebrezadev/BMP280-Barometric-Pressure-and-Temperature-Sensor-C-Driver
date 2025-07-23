a6220f98f4495644a8c827c68a08b70c747781fa
Reza Ebrahimi <ebrahimi.reza88@gmail.com>
2025-07-23 05:41:31 +0330

    Arduino bug fix

--- example/arduino/interface.h
+++ example/arduino/interface.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-int bmp280_i2c_init();
-int bmp280_i2c_deinit();
+int bmp280_i2c_init(uint8_t deviceAddress);
+int bmp280_i2c_deinit(uint8_t deviceAddress);
 int bmp280_read_array(uint8_t deviceAddress, uint8_t startRegisterAddress, uint8_t *data, uint8_t dataLength);
 int bmp280_write_array(uint8_t deviceAddress, uint8_t startRegisterAddress, uint8_t *data, uint8_t dataLength);
 int delay_function(uint32_t delayMS);
